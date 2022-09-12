f4b0e6c415d42951527af3fcf2c1d52b02cfe555
welcome <welcome@DESKTOP-1KB0RC3>
2022-09-12 12:49:35 +0530

    Commit

---
+++ src/test/java/demo/CreateContacts.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class CreateContacts {
+	@Test
+	public void createNewContacts() {
+		System.out.println("Contacts created");
+	}
+	
+}
---
+++ src/test/java/demo/CreateOrgTest.java
@@ -0,0 +1,33 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	
+	@Test
+	public void createNewOrg() {
+		System.out.println("org created");
+	}
+	
+
+	@Test
+	public void modifyNewOrg() {
+		System.out.println("modify");
+	}
+	
+	
+
+	@Test
+	public void deletedOrg() {
+		System.out.println("deleted");
+	}
+
+
+	@Test
+	public void demo() {
+		System.out.println("executed");	
+}
+
+}
+
